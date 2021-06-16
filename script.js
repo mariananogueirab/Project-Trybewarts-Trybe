5a99168ceb7dfe22a1ac49c3a8635e30aef5594b
mariananogueirab <mariananbp@gmail.com>
2021-06-16 19:29:42 -0300

    Requisitos 2 e 3

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const botao = document.querySelector('#botao-id');
+
+botao.addEventListener('click', () => {
+  const email = document.querySelector('#email-id').value;
+  const senha = document.querySelector('#senha-id').value;
+  if (email === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Login ou senha inválidos.');
+  }
+
+})
